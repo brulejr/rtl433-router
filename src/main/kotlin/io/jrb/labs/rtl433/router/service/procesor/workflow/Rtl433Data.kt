55d930b3fc6e78810fa5b2e126151512284b4c3b
Jon Brule <brulejr@tgmail.com>
2025-02-25 19:08:13 -0500

    Refactors Rtl433Data class

--- src/main/kotlin/io/jrb/labs/rtl433/router/service/procesor/workflow/Rtl433Data.kt
+++ src/main/kotlin/io/jrb/labs/rtl433/router/service/procesor/workflow/Rtl433Data.kt
@@ -2,8 +2,10 @@ package io.jrb.labs.rtl433.router.service.procesor.workflow
 
 import com.fasterxml.jackson.annotation.JsonAnyGetter
 import com.fasterxml.jackson.annotation.JsonAnySetter
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 import java.time.Instant
 
+@JsonDeserialize(builder = Rtl433Data.Builder::class)
 data class Rtl433Data(
     val type: String? = null,
     val area: String? = null,
@@ -11,19 +13,43 @@ data class Rtl433Data(
     val time: Instant,
     val model: String,
     val id: String,
-    val extraFields: MutableMap<String, Any?> = mutableMapOf()
+    val extraFields: Map<String, Any?> = mapOf()
 ) {
 
     operator fun contains(key: String): Boolean = extraFields.containsKey(key)
 
     operator fun get(key: String): Any? = extraFields[key]
 
-    @JsonAnySetter
-    private fun setProperty(key: String, value: Any?) {
-        extraFields[key] = value
-    }
-
     @JsonAnyGetter
     private fun getProperty(): Map<String, Any?> = extraFields
+
+    class Builder(private var model: String, private var id: String) {
+
+        var type: String? = null
+
+        var area: String? = null
+
+        var device: String? = null
+
+        var time: Instant = Instant.now()
+
+        var extraFields: MutableMap<String, Any?> = mutableMapOf()
+
+        fun build() = Rtl433Data(
+            type = type,
+            area = area,
+            device = device,
+            time = time,
+            model = model,
+            id = id,
+            extraFields = extraFields.toMap()
+        )
+
+        @JsonAnySetter
+        private fun setProperty(key: String, value: Any?) {
+            extraFields[key] = value
+        }
+
+    }
 
 }
