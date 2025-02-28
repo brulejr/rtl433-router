fc90d56c5850be8a67e5c69220d00a00df16f980
Jon Brule <brulejr@tgmail.com>
2025-02-28 08:57:29 -0500

    Adds MIT License header to all Kotlin classes

--- src/main/kotlin/io/jrb/labs/commons/eventbus/DataEvent.kt
+++ src/main/kotlin/io/jrb/labs/commons/eventbus/DataEvent.kt
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2024 Jon Brule <brulejr@gmail.com>
+ * Copyright (c) 2025 Jon Brule <brulejr@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
