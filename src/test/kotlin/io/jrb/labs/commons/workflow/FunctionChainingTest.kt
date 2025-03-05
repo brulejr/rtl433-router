8e68b6dd882f9819a08c754e5f90031f52b0c514
Jon Brule <brulejr@tgmail.com>
2025-03-05 17:34:24 -0500

    Enhances FunctionChainingTest

--- src/test/kotlin/io/jrb/labs/commons/workflow/FunctionChainingTest.kt
+++ src/test/kotlin/io/jrb/labs/commons/workflow/FunctionChainingTest.kt
@@ -3,6 +3,7 @@ package io.jrb.labs.commons.workflow
 import io.jrb.labs.commons.test.TestUtils
 import io.jrb.labs.commons.workflow.FunctionChainingTest.StepType1
 import io.jrb.labs.commons.workflow.FunctionChainingTest.StepType2
+import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -85,7 +86,7 @@ class FunctionChainingTest : TestUtils {
 
         val chain = steps.reduce { acc, nextfn -> StepType3 {
             acc.execute(it)
-            .takeIf { it is Outcome.Success }.let { it as Outcome.Success }.value
+            .takeIf { o -> o is Outcome.Success }.let { o -> o as Outcome.Success }.value
             .let(nextfn::execute)
         } }
 
@@ -97,14 +98,88 @@ class FunctionChainingTest : TestUtils {
             .hasFieldOrPropertyWithValue("sum", 11)
     }
 
+    @Test
+    fun `should concurrently execute a type 1 named numeric chain`() {
+        val steps = listOf(
+            StepType1Concurrent { c -> c.copy(sum = c.sum + 1) },
+            StepType1Concurrent { c -> c.copy(sum = c.sum * 2) },
+            StepType1Concurrent { c -> c.copy(sum = c.sum + 10) },
+            StepType1Concurrent { c -> c.copy(sum = c.sum / 2) }
+        )
+
+        val chain = steps.reduce { acc, nextfn -> StepType1Concurrent { nextfn(acc(it)) } }
+
+        runBlocking {
+            val result = chain(TestContext(sum = 6))
+
+            assertThat(result)
+                .hasFieldOrPropertyWithValue("sum", 12)
+        }
+    }
+
+    @Test
+    fun `should concurrently execute a type 2 named numeric chain`() {
+        val steps = listOf(
+            StepType2Concurrent { c -> c.copy(sum = c.sum + 1) },
+            StepType2Concurrent { c -> c.copy(sum = c.sum * 2) },
+            StepType2Concurrent { c -> c.copy(sum = c.sum + 10) },
+            StepType2Concurrent { c -> c.copy(sum = c.sum / 2) }
+        )
+
+        val chain = steps.reduce { acc, nextfn -> StepType2Concurrent { nextfn.execute(acc.execute(it)) } }
+
+        runBlocking {
+            val result = chain.execute(TestContext(sum = 5))
+
+            assertThat(result)
+                .hasFieldOrPropertyWithValue("sum", 11)
+        }
+    }
+
+    @Test
+    fun `should concurrently execute a type 3 named numeric chain - happy path`() {
+        val steps = listOf(
+            StepType3Concurrent { c -> Outcome.Success(c.copy(sum = c.sum + 1)) },
+            StepType3Concurrent { c -> Outcome.Success(c.copy(sum = c.sum * 2)) },
+            StepType3Concurrent { c -> Outcome.Success(c.copy(sum = c.sum + 10)) },
+            StepType3Concurrent { c -> Outcome.Success(c.copy(sum = c.sum / 2)) }
+        )
+
+        val chain = steps.reduce { acc, nextfn -> StepType3Concurrent {
+            nextfn.execute(
+                acc.execute(it)
+                    .takeIf { o -> o is Outcome.Success }.let { o -> o as Outcome.Success }.value
+            )
+        } }
+
+        runBlocking {
+            val result = chain.execute(TestContext(sum = 5))
+
+            assertThat(result)
+                .isInstanceOf(Outcome.Success::class.java)
+                .extracting("value")
+                .hasFieldOrPropertyWithValue("sum", 11)
+        }
+    }
+
     private fun interface StepType1 : (TestContext) -> TestContext
+
+    private fun interface StepType1Concurrent : suspend (TestContext) -> TestContext
 
     private fun interface StepType2 {
         fun execute(context: TestContext): TestContext
     }
 
+    private fun interface StepType2Concurrent {
+        suspend fun execute(context: TestContext): TestContext
+    }
+
     private fun interface StepType3 {
         fun execute(context: TestContext): Outcome<TestContext>
+    }
+
+    private fun interface StepType3Concurrent {
+        suspend fun execute(context: TestContext): Outcome<TestContext>
     }
 
 }
